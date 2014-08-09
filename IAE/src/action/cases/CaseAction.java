6164a1ed5ab66b7e45ee861a4d3a656030730d89
mattwitheridge <mattwitheridge@172.20.10.8>
2014-08-09 15:58:09 +1000

    3:57pm Saturday

--- IAE/src/action/cases/CaseAction.java
+++ IAE/src/action/cases/CaseAction.java
@@ -1,13 +1,411 @@
 package action.cases;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import action.base.BaseAction;
 
 public class CaseAction extends BaseAction {
+	
+	/* 
+	 * form title (can either be new enquiry/exisiting enquiry/enquiry list)
+	 */
+	String formTitle;;
+
+	/*
+	 * status
+	 */
+	Date createdDate;
+	Date updatedDate;
+	String createdBy;
+	String updatedBy;
+
+	Integer enquiryID;
+
+	//drop down menu
+	List<String> flagList; 			//Danger_Type table connected to Contacts table 
+	List<String> status; 			//Status_Type or criteria control value table 
+
+
+	/*
+	 * Personal Details
+	 */
+	List<String> title; String theTitle; 	//
+	List<String> gender; String theGender;	//
+	List<String> culturalBackground; String theCulturalBackground; //
+	Date birthDate;
+	String firstName;
+	String otherName;
+	String lastName;
+	String identification;
+	String culturalBackgroundComments; 	//for cultural background
+	String email; 						//need validation
+	String mobile;						//need validation
+
+	/*
+	 * Address 
+	 */
+	List<String> accomodation; 		//
+	String accomodationComments;
+	String address;
+	String city;
+	String state;
+	String country;
+	String postCode;
+	String homePhone;
+
+	/*
+	 * Referral
+	 */
+	String inquisitor;
+	String referredBy;
+	String referredTo;
+
+	/*
+	 * Employment
+	 */
+	String profession;
+	String workPhone;
+	String employmentDescription;
+	String employmentComment;
+
+	
+	
+	
 	public String newCase(){
+		System.out.println("in new case");
+		activateLists();
 		return SUCCESS;
 	}
 	
 	public String caseList(){
 		return SUCCESS;
+	}
+	
+	
+	private void activateLists(){
+		List<String> tit = new ArrayList<String>();
+		tit.add("Mr");
+		tit.add("Mrs");
+		setTitle(tit);
+		
+		List<String> gen = new ArrayList<String>();
+		gen.add("Male");
+		gen.add("Female");
+		setGender(gen);
+		
+		List<String> cult = new ArrayList<String>();
+		cult.add("aussie");
+		cult.add("oi");
+		setCulturalBackground(cult);
+		
+		List<String> acco = new ArrayList<String>();
+		acco.add("Acc1");
+		acco.add("Acc2");
+		setTitle(acco);
+	}
+	
+	
+	
+	
+	public String getFormTitle() {
+		return formTitle;
+	}
+	
+
+	public void setFormTitle(String formTitle) {
+		System.out.println(formTitle);
+		this.formTitle = formTitle;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Integer getEnquiryID() {
+		return enquiryID;
+	}
+
+	public void setEnquiryID(Integer enquiryID) {
+		this.enquiryID = enquiryID;
+	}
+
+	public List<String> getFlagList() {
+		return flagList;
+	}
+
+	public void setFlagList(List<String> flagList) {
+		this.flagList = flagList;
+	}
+
+	public List<String> getStatus() {
+		return status;
+	}
+
+	public void setStatus(List<String> status) {
+		this.status = status;
+	}
+
+	public List<String> getTitle() {
+		return title;
+	}
+
+	public void setTitle(List<String> title) {
+		this.title = title;
+	}
+
+	public List<String> getGender() {
+		return gender;
+	}
+
+	public void setGender(List<String> gender) {
+		this.gender = gender;
+	}
+
+	public List<String> getCulturalBackground() {
+		return culturalBackground;
+	}
+
+	public void setCulturalBackground(List<String> culturalBackground) {
+		this.culturalBackground = culturalBackground;
+	}
+
+	
+	
+	public String getTheTitle() {
+		return theTitle;
+	}
+
+	public void setTheTitle(String theTitle) {
+		this.theTitle = theTitle;
+	}
+
+	public String getTheGender() {
+		return theGender;
+	}
+
+	public void setTheGender(String theGender) {
+		this.theGender = theGender;
+	}
+
+	public String getTheCulturalBackground() {
+		return theCulturalBackground;
+	}
+
+	public void setTheCulturalBackground(String theCulturalBackground) {
+		this.theCulturalBackground = theCulturalBackground;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getOtherName() {
+		return otherName;
+	}
+
+	public void setOtherName(String otherName) {
+		this.otherName = otherName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getIdentification() {
+		return identification;
+	}
+
+	public void setIdentification(String identification) {
+		this.identification = identification;
+	}
+
+	public String getCulturalBackgroundComments() {
+		return culturalBackgroundComments;
+	}
+
+	public void setCulturalBackgroundComments(String culturalBackgroundComments) {
+		this.culturalBackgroundComments = culturalBackgroundComments;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String string) {
+		this.mobile = string;
+	}
+
+	public List<String> getAccomodation() {
+		return accomodation;
+	}
+
+	public void setAccomodation(List<String> accomodation) {
+		this.accomodation = accomodation;
+	}
+
+	public String getAccomodationComments() {
+		return accomodationComments;
+	}
+
+	public void setAccomodationComments(String accomodationComments) {
+		this.accomodationComments = accomodationComments;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getHomePhone() {
+		return homePhone;
+	}
+
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+
+	public String getInquisitor() {
+		return inquisitor;
+	}
+
+	public void setInquisitor(String inquisitor) {
+		this.inquisitor = inquisitor;
+	}
+
+	public String getReferredBy() {
+		return referredBy;
+	}
+
+	public void setReferredBy(String referredBy) {
+		this.referredBy = referredBy;
+	}
+
+	public String getReferredTo() {
+		return referredTo;
+	}
+
+	public void setReferredTo(String referredTo) {
+		this.referredTo = referredTo;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+	public String getWorkPhone() {
+		return workPhone;
+	}
+
+	public void setWorkPhone(String workPhone) {
+		this.workPhone = workPhone;
+	}
+
+	public String getEmploymentDescription() {
+		return employmentDescription;
+	}
+
+	public void setEmploymentDescription(String employmentDescription) {
+		this.employmentDescription = employmentDescription;
+	}
+
+	public String getEmploymentComment() {
+		return employmentComment;
+	}
+
+	public void setEmploymentComment(String employmentComment) {
+		this.employmentComment = employmentComment;
 	}
 }
